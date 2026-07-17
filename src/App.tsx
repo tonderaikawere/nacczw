d3ea122168ca0c3be384468f79bfc11da5f5d2cb
tonderaikawere <tondeskawere@gmail.com>
2026-07-17 22:24:47 +0200

    design: wrap App in ThemeProvider and CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // National Association of Christian Churches App Router
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme';
 import Header from './components/Header';
 import AppDownload from './components/AppDownload';
 
@@ -10,26 +12,27 @@ import Services from './pages/Services/Services';
 import Blog from './pages/Blog/Blog';
 import Contact from './pages/Contact/Contact';
 import OurGallery from './pages/Gallery/OurGallery';
-
 import Donation from './pages/Donate/Donate';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/gallery" element={<OurGallery />} />
-        
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/donate" element={<Donation />} />
-        <Route path="/app-download" element={<AppDownload />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/gallery" element={<OurGallery />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/donate" element={<Donation />} />
+          <Route path="/app-download" element={<AppDownload />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
